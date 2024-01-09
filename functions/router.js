52fc302f8946d0c69b97d2d3d798a64a5bfde799
Antoine Nouger <antoine.nouger@mrjaffesclass.com>
2024-01-09 14:09:53 -0800

    index out of bound finish & episodeAirTime route added

--- functions/router.js
+++ functions/router.js
@@ -1,8 +1,10 @@
 const episodeIndex = require('./routes/episodeIndex');
 const router = require('express').Router();
 const episodes = require('./routes/episodes');
+const episodeAirTime = require('./routes/episodeAirTime');
 
 router.get("/episodes", episodes);
 router.get("/episode-index/:index", episodeIndex); 
+router.get("/airtime/:airtime", episodeAirTime);
 
 module.exports = router;
--- functions/routes/episodeIndex.js
+++ functions/routes/episodeIndex.js
@@ -1,11 +1,26 @@
 const bbt = require('big-bang-theory');
 
 const episodeIndex = (req, res) => {
+
+  if(req.params.index < 279 && req.params.index > 0){
   res.status(200).json({
     success: true,
     index: req.params.index,
     data: bbt._embedded.episodes[req.params.index]
   });
 }
+else {
+  console.log("index out of bounds")
+  res.status(200).json({
+    success: false,
+    index: req.params.index,
+    message: "index value out of range. There are only 279 episodes of this show"
+  })
+}
+}
+
+
+
+console.log("working")
 
 module.exports = episodeIndex;
